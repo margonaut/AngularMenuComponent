2c9d13c12e774b92f246d04b0e31a7a5636579dd
Margaret Morgan <mmogan@datto.com>
2016-06-12 18:37:09 -0400

    Second draft menu item DDO built out

--- app/components/menu-item/menu-item.directive.js
+++ app/components/menu-item/menu-item.directive.js
@@ -11,6 +11,20 @@ angular.
         text: '@',
         url: '@'
       },
-      controller: ['$scope']
+      controller: ['$scope', function($scope, $element, $attrs) {
+        
+        // trying out selection action pattern
+        $scope.selected = function($event, scope) {
+          
+          $scope.emit('menu-item-selected', scope);
+          
+          // maybe we can set custom actions here?
+          // or at least this is where we can call them
+        };
+      }],
+      link: function(scope, iElement, iAttrs) {
+        
+        // this is where the pattern sets menu items to be disabled
+      }
     }
   });
