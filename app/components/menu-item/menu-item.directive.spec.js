f3a8f0f1762c352aa73eda6d9c54d75caf8e3641
Margaret Morgan <mmogan@datto.com>
2016-06-13 11:38:06 -0400

    More test setup work

--- app/components/menu-item/menu-item.directive.spec.js
+++ app/components/menu-item/menu-item.directive.spec.js
@@ -1,40 +1,46 @@
-'use strict';
+// 'use strict';
 
 describe('menuItem directive', function() {
+  var element, scope;
   
-  var $compile, $rootScope, $scope, $element, element, $event;
   // Load the wrapper module that contains the menuItem directive
   beforeEach(module('menuItem'));
   
-  // Grab environment and angular utilities
-  beforeEach(inject(function(_$compile_, _$rootScope_) {
-    $compile = _$compile_;
-    $rootScope = _$rootScope_;
-    $scope = $rootScope.$new();
-  }));
-  
-  var testItem = '<menu-item text="Bold" helper-text="Cmd+B" ng-click="menu.boldText($event)"></menu-item>'
-  
-  // Insert our HTML and compile the directive
-  function CompileDirective(directiveHtml) {
-    if(!directiveHtml) directiveHtml = testItem;
-    inject(function($compile) {
-      $element = $compile(directiveHtml)($scope);
-      $scope.$digest();
-      element = $element.html();
-    })
-  }
-  
-  describe('menuItem', function() {
-    var scope;
-    beforeEach(function() {
-      $log(scope)
-      scope = $element.data('$scope').$$childHead;
-      // $event = $.Event("click");
-    });
+  beforeEach(inject(function($rootScope, $compile) {
+    scope = $rootScope.$new();
     
-    it('should display the text attribute', function() {
-      expect(element).toContain("Bold");
-    })
-  })
+    element = '<menu-item text="Bold" helper-text="Cmd+B"></menu-item>';
+    
+    element = $compile(element)(scope);
+    scope.$digest();
+    
+  }))
+  
+  describe('it should display the text attribue', function() {
+    // done();
+    expect(element).toBe(3);
+  });
+  
+  // // Insert our HTML and compile the directive
+  // function CompileDirective(directiveHtml) {
+  //   if(!directiveHtml) directiveHtml = testItem;
+  //   inject(function($compile) {
+  //     $element = $compile(directiveHtml)($scope);
+  //     $scope.$digest();
+  //     element = $element.html();
+  //   })
+  // }
+  
+  // describe('menuItem', function() {
+  //   // var scope;
+  //   beforeEach(function() {
+  //     
+  //     // scope = $element.data('$scope').$$childHead;
+  //     // $event = $.Event("click");
+  //   });
+  //   
+  //   it('should display the text attribute', function() {
+  //     expect(element).toContain("Bold");
+  //   })
+  // })
 });
