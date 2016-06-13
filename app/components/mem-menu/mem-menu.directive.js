07e766bab7045937d175f4911b20ad9795516baa
Margaret Morgan <mmogan@datto.com>
2016-06-13 15:20:30 -0400

    Refactoring to use link instead of compile, actions working

--- app/components/mem-menu/mem-menu.directive.js
+++ app/components/mem-menu/mem-menu.directive.js
@@ -6,19 +6,44 @@ angular.
     return {
       templateUrl: 'components/mem-menu/mem-menu.template.html',
       restrict: 'E',
-      // scope: {
-      //   
-      // },
+      scope: {
+        itemAction: '&'
+      },
       // replace: true,
       transclude: true,
-      compile: function(tElement, attrs, transclude) {
-        return function($scope) {
-          transclude($scope, function(clone) {
-            tElement.append(clone);
-          })
-        };
+      // compile: function(tElement, attrs, transclude) {
+      //   return function($scope) {
+      //     transclude($scope, function(clone) {
+      //       tElement.append(clone);
+      //     })
+      //   };
+      // },
+      link: function(scope, elem, attr, ctrl, transclude) {
+        transclude(function(clone) {
+          var triggerButton = "<button ng-click='ctrl.toggleMenu($event)'>Let's Go</button>";
+          var menuContent = "<mem-menu-content></mem-menu-content>";
+          // var menuContentEnd = "</mem-menu-content>"
+          // menuContent.append(clone);
+          elem.append(triggerButton);
+          elem = elem.append(menuContent);
+          elem.append(clone);
+          // elem.append(menuContent);
+          
+          // elem.append(clone);
+          // debugger;
+          // var menuContent.innerHTML = clone;
+          // elem.innerHTML = elem.innerHTML + triggerButton + menuContent;
+          // elem.append(clone);
+          // debugger;
+        })
       },
       controller: ['$scope', function($scope, $elements, $attrs) {
+        // bad area for dom manipulation, good for sharing scope
+        
+        var test = function() {
+          debugger;
+        }
+        
         var findMenu = function(event) {
           return event.target.nextElementSibling;
         };
