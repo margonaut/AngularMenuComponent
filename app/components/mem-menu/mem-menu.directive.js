ac952dd71efee54bc8a55ac21815b7af1c0588ab
Margaret Morgan <mmogan@datto.com>
2016-06-12 22:29:55 -0400

    Added clone pattern to transclude menu content

--- app/components/mem-menu/mem-menu.directive.js
+++ app/components/mem-menu/mem-menu.directive.js
@@ -6,7 +6,15 @@ angular.
     return {
       templateUrl: 'components/mem-menu/mem-menu.template.html',
       restrict: 'E',
+      // replace: true,
       transclude: true,
+      compile: function(tElement, attrs, transclude) {
+        return function($scope) {
+          transclude($scope, function(clone) {
+            tElement.append(clone);
+          })
+        };
+      },
       scope: {},
       controller: ['$scope', function($scope, $elements, $attrs) {
         
