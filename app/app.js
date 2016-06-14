bd71252f2435aeee23d9ef896af228016a95ab26
Margaret Morgan <mmogan@datto.com>
2016-06-14 11:35:33 -0400

    Remove unused code

--- app/app.js
+++ app/app.js
@@ -2,28 +2,10 @@
 
 // Declare app level module which depends on views, and components
 var app = angular.module('myApp', [
-  'ngRoute',
   'menuItem',
   'memMenu'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
+]).controller('MenuController', ['$scope', function($scope) {
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
-
-app.controller('MenuController', ['$scope', function($scope) {
-  this.menuTwo = [
-    {
-      text: "Trying it again",
-      helperText: "Cmd+P"
-    },
-    {
-      text: "Second time",
-      helperText: "Cmd+D"
-    }
-  ];
-  
   this.printPage = function(event) {
     alert("This is the print event");
   };
@@ -40,26 +22,4 @@ app.controller('MenuController', ['$scope', function($scope) {
     alert("Generic Action to the rescue!")
   };
   
-  this.items = [
-    {
-      text: 'Thing One',
-      helpText: 'Cmd+P',
-      action: function() {
-        alert('Contact');
-      }
-    },
-    {
-      text: 'Thing Two',
-      action: function() {
-        alert('This is thing two\'s action');
-      }
-    },
-    {
-      text: 'Threee',
-      helpText: 'Cmd+Q',
-      action: function() {
-        alert('Thing three is here');
-      }
-    },
-  ]
 }]);
--- app/menu.controller.js
+++
@@ -1,5 +0,0 @@
-// angular.module('MenuController').
-//   controller('MenuController', ['$scope', function($scope) {
-//     $scope.greeting = 'Hola!';
-//     this.welcome = "WHAT";
-//   }]);
